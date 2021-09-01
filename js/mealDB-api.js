066bafac27731756fa4d3adb110c523787012f54
sohag-aust <frustratedcoder96@gmail.com>
2021-09-02 03:17:42 +0600

    grid display : www.themealdb.com/api

--- js/mealDB-api.js
+++ js/mealDB-api.js
@@ -8,7 +8,40 @@ const searchFood = () => {
 
     fetch(url)
         .then((res)=>res.json())
-        .then((data) => console.log(data.meals));
+        .then((data) => displaySearchResult(data.meals));
+}
 
+const displaySearchResult = (meals) => {
 
+    const searchResult = document.getElementById('search-result');
+
+    meals.forEach((meal) => {
+        console.log(meal);
+
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        const col = document.createElement('div');
+        col.classList.add('col-lg-4');
+
+        row.appendChild(col);
+
+        col.innerHTML = `
+            
+            <div class="card h-100 m-4">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="image is missing">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0,200)} </p>
+                </div>
+            
+                <div class="card-footer mb-4">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            
+        `;
+
+        searchResult.appendChild(col);
+    })
 }
