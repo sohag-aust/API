9d166c82fbbd0ca54e8752afdacf9c6797a8f079
sohag-aust <frustratedcoder96@gmail.com>
2021-09-02 02:27:57 +0600

    refactor : restcountries.eu/rest/v2/all

--- js/restCountries-api.js
+++ js/restCountries-api.js
@@ -7,25 +7,38 @@ const loadCountries = () => {
 loadCountries();
 
 const displayCountry = (countries) => {
-    console.log(countries);
-
-
+    
     const displayDiv = document.getElementById('country');
 
     countries.forEach((country) => {
-        console.log(country);
-
-        const h2 = document.createElement('h2');
-        const p = document.createElement('p');
-
-        h2.innerText = `Name : ${country.name}`;
-        p.innerText = `Capital : ${country.capital}`;
 
         const innerDiv = document.createElement('div');
         innerDiv.classList.add('design');
-        innerDiv.appendChild(h2);
-        innerDiv.appendChild(p);
+        innerDiv.innerHTML = `
+            <h2> Name : ${country.name} </h2>
+            <p> Capital : ${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name}')"> Show Details </button>
+        `;
 
         displayDiv.appendChild(innerDiv);
     })
+}
+
+const loadCountryByName = (countryName) => {
+    const url = `https://restcountries.eu/rest/v2/name/${countryName}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayCountryDetails(data[0]));
+}
+
+const displayCountryDetails = (countryDetails) => {
+    console.log(countryDetails);
+
+    const countryDetailsDiv = document.getElementById('country-detail');
+
+    countryDetailsDiv.innerHTML = `
+        <h3>Name : ${countryDetails.name}</h3>
+        <h5>Population : ${countryDetails.population}</h5>
+        <img style="width:200px;" src="${countryDetails.flag}">
+    `;
 }
